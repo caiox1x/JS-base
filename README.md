46c2787f4ec907f2b53a1354183422b728900274
caiox1x <caiodocarmo00@gmail.com>
2022-08-15 23:33:45 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS-base
 Estou aprendendo TS, senti a necessidade de aprender a base do JS de maneira mais aprofundada, vou usar esse repo para postar os exemplos das minhas aulas 
+
+Quero aprofundar o conhecimento em coisas "básicas" como lógica de programação, funções, array e seus metodos, consumos de API antes de dar de fato o próximo passo.
---
+++ aula 1 - conceitos iniciais/treino.js
@@ -0,0 +1,21 @@
+// funções e arrow functions
+
+//const soma = (x, y) => {
+//   return  x + y
+//};
+
+const raiz = (n) => {
+    return n ** 0.5;
+};
+
+//outra forma de fazer
+function soma(x, y){
+    return x + y;
+};
+
+//arrow function mais resumida 
+
+const soma = (x, y) => x + y // caso use só um parametro nn é necessario paresentese
+
+console.log(soma(2, 5) + soma(10, 10)); 
+console.log(raiz(16).toFixed()); //lembrar de usar toFixed pra remover as dizimas 
