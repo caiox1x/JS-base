ddbf7d5802f94bc5f24c085767788a83607dea6a
caiox1x <caiodocarmo00@gmail.com>
2022-08-17 04:30:03 -0300

    arrays

--- aula 2 - lógica/arrays.js
+++ aula 2 - lógica/arrays.js
@@ -1,7 +1,16 @@
 // atribuição via desestruturação com
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeros = [1414516, 2165165, 3, 4, 5, 6, 7, 8, 9];
 
-const [numeroUm, numeroDois] = numeros;
+const [numeroUm, numeroDois, ...resto ] = numeros;
 
-console.log(numeroUm, numeroDois );
+resto.splice(1, 0, 2255)
+
+console.log(numeroUm, numeroDois, resto);
+console.log(numeros[1]);
+
+const arrayDeArrays = [[1, 2 ,3], [4, 5 ,6], [7, 8 ,9]]
+
+const [,[,,seis]] =  arrayDeArrays;
+
+console.log(seis);
